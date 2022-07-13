2aa0f34e43390ed2bd1248a19d28eafa6703365d
stefano-code <101890417+stefano-code@users.noreply.github.com>
2022-07-13 14:08:33 +0200

    Delete imcplib directory

--- imcplib/src/main/java/com/android/imcplib/Exercise.kt
+++
@@ -1,9 +0,0 @@
-package com.android.imcplib
-
-import android.content.Context
-import android.net.Uri
-import com.android.imcp.api.IMCPProxy
-
-class Exercise (ctx : Context) : IMCPProxy(ctx, Uri.parse("content://com.android.imcp.Exercise.AUTHORITY/item")) {
-}
-
--- imcplib/src/main/java/com/android/imcplib/Observer.kt
+++
@@ -1,6 +0,0 @@
-package com.android.imcp.api
-
-interface Observer {
-    fun update()
-}
-
